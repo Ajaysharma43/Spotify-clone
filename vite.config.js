c92b451f8f82f240f38b69c4e59a24a12caf3146
Ajay sharma <139312719+Ajaysharma43@users.noreply.github.com>
2024-07-25 15:53:31 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Your backend API URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
+
+
