c32bfa41fc22fd0fea4914ef3e60a67696f5485b
Ajay sharma <139312719+Ajaysharma43@users.noreply.github.com>
2024-08-23 22:43:54 +0530

    Add files via upload

--- src/Components/songlist/songlist.jsx
+++ src/Components/songlist/songlist.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeSong } from '../../features/songs/songslice';
-import { Link,useNavigate } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
-import { FaRegHeart, FaSun, FaMoon } from 'react-icons/fa';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeSong } from "../../features/songs/songslice";
+import { Link, useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaRegHeart, FaSun, FaMoon } from "react-icons/fa";
+import axios from "axios";
 
 const linkUrl = import.meta.env.VITE_API_URL;
 
@@ -19,34 +19,40 @@ function Likedsongs() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const Username = sessionStorage.getItem('Username');
-        const Password = sessionStorage.getItem('Password');
-        const response = await axios.post(`${linkUrl}/GetUserData`, { Username, Password });
+        const Username = sessionStorage.getItem("Username");
+        const Password = sessionStorage.getItem("Password");
+        const response = await axios.post(`${linkUrl}/GetUserData`, {
+          Username,
+          Password,
+        });
         setUserData(response.data);
         console.log(userData);
-        
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
     fetchData();
   }, []);
 
-  const removeSongHandler = async(song) => {
+  const removeSongHandler = async (song) => {
     const id = song.id;
     const name = song.name;
     const data = song.Song;
-    const Username = sessionStorage.getItem('Username');
-      const Password = sessionStorage.getItem('Password');
-      
-      const response = await axios.post(`${linkUrl}/RemoveLikedSongs`,{id,name,data,Username,Password})
-      console.log(response.data);
-      if(response.data == 'removed')
-      {
-        
-        navigate(0);
-      }
+    const Username = sessionStorage.getItem("Username");
+    const Password = sessionStorage.getItem("Password");
+
+    const response = await axios.post(`${linkUrl}/RemoveLikedSongs`, {
+      id,
+      name,
+      data,
+      Username,
+      Password,
+    });
+    console.log(response.data);
+    if (response.data == "removed") {
+      navigate(0);
+    }
   };
 
   const toggleDarkMode = () => {
@@ -60,14 +66,18 @@ function Likedsongs() {
       transition={{ duration: 0.5, ease: "easeOut" }}
       exit={{ opacity: 0 }}
       className={`min-h-screen flex flex-col items-center justify-center transition-all duration-500 ${
-        darkMode ? 'bg-gradient-to-br from-gray-900 to-black' : 'bg-gradient-to-br from-purple-600 to-indigo-600'
+        darkMode
+          ? "bg-gradient-to-br from-gray-900 to-black"
+          : "bg-gradient-to-br from-purple-600 to-indigo-600"
       }`}
     >
       <div className="max-w-7xl w-full mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg transition-all duration-500">
         <div className="flex justify-end mb-4">
           <motion.button
             onClick={toggleDarkMode}
-            className={`px-4 py-2 rounded-lg ${darkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-gray-800'} hover:bg-opacity-75 focus:outline-none`}
+            className={`px-4 py-2 rounded-lg ${
+              darkMode ? "bg-gray-800 text-white" : "bg-gray-200 text-gray-800"
+            } hover:bg-opacity-75 focus:outline-none`}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             transition={{ duration: 0.3 }}
@@ -75,7 +85,13 @@ function Likedsongs() {
             {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
           </motion.button>
         </div>
-        <h1 className={`text-3xl font-bold ${darkMode ? 'text-black' : 'text-gray-800'} mb-6`}>Your Liked Songs</h1>
+        <h1
+          className={`text-3xl font-bold ${
+            darkMode ? "text-black" : "text-gray-800"
+          } mb-6`}
+        >
+          Your Liked Songs
+        </h1>
         <AnimatePresence>
           {userData && userData.Likedsongs.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -84,7 +100,9 @@ function Likedsongs() {
                   key={item.id}
                   ref={(el) => (cardRefs.current[index] = el)}
                   className={`p-4 rounded-lg shadow-md transition-all duration-500 ${
-                    darkMode ? 'bg-gradient-to-t from-gray-700 to-gray-900' : 'bg-gradient-to-t from-white to-gray-200'
+                    darkMode
+                      ? "bg-gradient-to-t from-gray-700 to-gray-900"
+                      : "bg-gradient-to-t from-white to-gray-200"
                   }`}
                   initial={{ opacity: 0, y: -20 }}
                   animate={{ opacity: 1, y: 0 }}
@@ -92,12 +110,25 @@ function Likedsongs() {
                   transition={{ duration: 0.3 }}
                 >
                   <Link to={`/single/${item.id}`} className="block mb-2">
-                  <img src={item.Image} className='w-full h-full'/>
-                    <h2 className={`text-xl font-semibold ${darkMode ? 'text-white' : 'text-gray-800'}`}>{item.name}</h2>
+                    <motion.img
+                      src={item.Image}
+                      className="w-full h-full object-cover rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl"
+                      alt={item.name}
+                      whileHover={{ scale: 1.05 }}
+                    />
+                    <h2
+                      className={`text-xl font-semibold ${
+                        darkMode ? "text-white" : "text-gray-800"
+                      }`}
+                    >
+                      {item.name}
+                    </h2>
                   </Link>
                   <motion.button
                     onClick={() => removeSongHandler(item)}
-                    className={`bg-red-500 transition duration-300 hover:bg-red-600 text-white py-2 px-4 rounded-lg focus:outline-none flex items-center mt-2 ${darkMode ? 'bg-opacity-75' : ''}`}
+                    className={`bg-red-500 transition duration-300 hover:bg-red-600 text-white py-2 px-4 rounded-lg focus:outline-none flex items-center mt-2 ${
+                      darkMode ? "bg-opacity-75" : ""
+                    }`}
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
                     transition={{ duration: 0.3 }}
@@ -114,9 +145,11 @@ function Likedsongs() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.3 }}
-              className={`text-lg ${darkMode ? 'text-black' : 'text-gray-800'}`}
+              className={`text-lg ${darkMode ? "text-black" : "text-gray-800"}`}
             >
-              {userData && userData.Likedsongs.length === 0 ? 'You haven\'t liked any songs yet.' : 'Loading...'}
+              {userData && userData.Likedsongs.length === 0
+                ? "You haven't liked any songs yet."
+                : "Loading..."}
             </motion.p>
           )}
         </AnimatePresence>
